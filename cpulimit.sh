a5331dbd5cfedd308fa1eb97f8bc680dcc92464a
kasimridvan <ridvan@eftsoftware.com>
2017-07-21 20:47:00 +0300

    r

---
+++ cpulimit.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+for i in {1..9999999999999999}
+do
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 20
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 50
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 60
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 15
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 20
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 50
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 70
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 90
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 70
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 60
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 20
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 50
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 60
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 80
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 20
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 55
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 75
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 50
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 65
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 88
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 75
+  sleep 1s
+  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 60
+  sleep 1s
+done
