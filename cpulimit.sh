8194ae8c2fb5f1dbe2fb583822cf245e72ffd0e3
kasimridvan <ridvan@eftsoftware.com>
2017-07-21 23:19:58 +0300

    r

--- cpulimit.sh
+++ cpulimit.sh
@@ -1,60 +1,60 @@
 #!/bin/bash
 for i in {1..9999999999999999}
 do
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 20
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 20
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 40
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 50
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 50
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 40
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 60
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 60
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 15
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 15
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 20
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 20
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 40
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 50
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 50
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 70
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 70
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 90
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 90
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 40
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 70
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 70
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 60
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 60
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 20
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 20
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 40
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 50
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 50
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 60
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 60
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 80
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 80
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 20
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 20
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 40
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 40
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 55
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 55
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 75
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 75
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 50
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 50
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 65
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 65
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 88
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 88
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 75
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 75
   sleep 1s
-  cpulimit -P /home/python/cpuminer-easy/cpuminer -l 60
+  cpulimit -P /home/ubuntu/cpuminer-easy/cpuminer -l 60
   sleep 1s
 done
